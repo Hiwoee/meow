6ad581c2462d28d9d9553d9f9f1bf10a7bdb7760
Hiro <118013846+Hiwoee@users.noreply.github.com>
2024-01-30 01:50:51 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -129,16 +129,10 @@ export default function Home() {
                         <svg
                           className="text-red h-6 transition-all"
                           viewBox="0 0 32 32"
-                          fill="#E60023"
+                          fill="#000000"
                           xmlns="http://www.w3.org/2000/svg"
                         >
-                          <Image
-                    className="h-64 w-64 rounded-full border-4 border-solid border-primary bg-primary object-cover"
-                    src="/profile.png"
-                    alt="kofi"
-                    width={100}
-                    height={100}
-                  />
+                          <path d="M371 140H100c-18 0-20 20 -20 20v184s3 40 40 40h188c41 0 40-40 40 -40v-26h23a89 89 0 000-178m0 132h-23v-86h23a43 43 0 010 86"></path><path d="M286 219c-10-30-63-33-77 3-15-36-67-33-77-3-7 19-4 35 9 54s68 64 68 64 56-45 68-64 15-35 9-54"></path>
                           <title>Ko-fi</title>
                         </svg>
                       </a>
