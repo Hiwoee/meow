075b4744ea3924295762bd158495cce0ea584b86
Hiro <118013846+Hiwoee@users.noreply.github.com>
2024-01-30 02:13:43 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -131,26 +131,10 @@ export default function Home() {
                           viewBox="0 0 32 32"
                           fill="#000000"
                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M371 140H100c-18 0-20 20 -20 20v184s3 40 40 40h188c41 0 40-40 40 -40v-26h23a89 89 0 000-178m0 132h-23v-86h23a43 43 0 010 86"></path><path d="M286 219c-10-30-63-33-77 3-15-36-67-33-77-3-7 19-4 35 9 54s68 64 68 64 56-45 68-64 15-35 9-54"></path>
-                          <title>Ko-fi</title>
-                        </svg>
-                      </a>
-                      <a
-                        rel="noopener noreferrer nofollow"
-                        target="_blank"
-                        href="/ko-fi"
-                        className="flex cursor-pointer items-center justify-center rounded-full border-white border-opacity-10 bg-opacity-10 p-1.5 transition-all"
-                      >
-                        <svg
-                          className="text-red h-6 transition-all"
-                          viewBox="0 0 32 32"
-                          fill="#000000"
-                          xmlns="http://www.w3.org/2000/svg"
         
                         >
                           <title>Ko-fi</title>
-                          <image href="/assests/kofi.svg" width="100%" height="100%" />
+                          <path d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734c4.352.24 7.422-2.831 6.649-6.916m-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09c-.443-.441-3.368-3.049-4.034-3.954c-.709-.965-1.041-2.7-.091-3.71c.951-1.01 3.005-1.086 4.363.407c0 0 1.565-1.782 3.468-.963c1.904.82 1.832 3.011.723 4.311m6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015"/>
                         </svg>
                       </a>
                       <div className="flex items-center">
