14391bc93f0b1f217854107a891ca8f448601574
Hiro <118013846+Hiwoee@users.noreply.github.com>
2024-02-03 14:53:10 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,21 +26,19 @@
 
     return (
       <>
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
+         <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-JVJ7735WX9"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-          `}
-        </Script>
+          gtag('config', 'G-JVJ7735WX9');
+        `}
+      </Script>
 
         <Analytics />
 
